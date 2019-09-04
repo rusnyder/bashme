86bbcd714a75e0633774efc9609b88b721c5e01e
Russell Snyder <ru.snyder@gmail.com>
2019-09-04 16:22:09 -0400

    Added github-markdown-toc script to path

---
+++ modules/github_markdown_toc/main.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# Load the github-markdown-toc submodule
+load_git_submodule github-markdown-toc
+
+# Alias it for global usage
+load_bin github_markdown_toc github-markdown-toc/gh-md-toc
